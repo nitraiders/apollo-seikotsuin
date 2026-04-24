5833bee523732d64199e7d92c262a43fc6545ef4
Kazuyoshi Miyamoto <nitraiders2106@gmail.com>
2026-04-24 17:39:19 +0900

    Clean upload: HPB clone version (8 pages + images)

--- upload_v2.ps1
+++
@@ -1,6 +0,0 @@
-$bytes = [System.IO.File]::ReadAllBytes("deploy_final_v2.php")
-$data = [Convert]::ToBase64String($bytes)
-$url = "https://apollo-seikotsuin.jp/u.php"
-$body = @{ d = $data }
-$response = Invoke-RestMethod -Uri $url -Method Post -Body $body
-Write-Host "Response: $response"
